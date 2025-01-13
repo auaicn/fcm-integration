271a2830d1bb01ab1e3513e8b4eb1c8e140cbb30
KyungHo Park <crorowslowpp@gmail.com>
2025-01-13 14:06:55 +0900

    chore: added dotenv and .env sample file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { config } from 'dotenv';
+
+config();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  console.table({
+    'process.env.DEVELOPMENT_ENVIRONMENT_SERVICE_ACCOUNT_KEY_FILE_PATH':
+      process.env.DEVELOPMENT_ENVIRONMENT_SERVICE_ACCOUNT_KEY_FILE_PATH,
+    'process.env.OPERATION_ENVIRONMENT_SERVICE_ACCOUNT_KEY_FILE_PATH':
+      process.env.OPERATION_ENVIRONMENT_SERVICE_ACCOUNT_KEY_FILE_PATH,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
